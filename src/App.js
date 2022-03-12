bf0caac1b97dd8e047f2b8585d49ebb58a7f419e
darksteel101 <71864703+darksteel101@users.noreply.github.com>
2022-03-12 08:22:56 -0500

    created operational search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="mt-4">Hello World</header>
+      <main className="container">
+        <header className="mt-4">Hello World</header>
+        <Dictionary />
+      </main>
+      <footer className="footer">
+        <small>Coded by Rebecca Leep</small>
+      </footer>
     </div>
   );
 }
