47b4c2ef7097acd8e2c4752f8fb1be23df272adc
darksteel101 <71864703+darksteel101@users.noreply.github.com>
2022-03-19 10:34:46 -0400

    styled app and added default search results

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <main className="container">
-        <h1 className="mt-4">Dictionary</h1>
+        <h1 className="mt-4">What word do you want to look up?</h1>
         <Dictionary defaultKeyword="sunset" />
       </main>
       <footer className="footer">
